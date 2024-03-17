90fa39756443d7bf492096988ec4132da5df50e3
f0Fx <luka2317@its.edu.rs>
2024-03-17 11:48:02 +0100

    File structure added

---
+++ client/source.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    // Allow the client to connect to the server
+    // Check if client is subscribed to the service
+    // If yes, download encrypted .dll into memory
+    // Upon injection, the .dll will decrypt line by line and execute to avoid detection
+    
+    return  0;
+}
